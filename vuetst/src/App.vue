2164d4d146efa9a3ff788bbf33f909690b714578
sseho <96410921+sseho@users.noreply.github.com>
2021-12-28 13:11:35 +0900

    Update App.vue

--- vuetst/src/App.vue
+++ vuetst/src/App.vue
@@ -12,22 +12,23 @@
     </div>
   </div>
 
-
+<div>
+  <h1>더 주 (The 酒)</h1>
+</div>
   <div class="menu">
     <a v-for="(a,i) in 메뉴들" :key="i"> {{a}} </a>
   </div>
   <hr>
   
-  <div v-for="(a,i) in 원룸들" :key="i">
+  <div v-for="(a,i) in 원룸들" :key="i" class="jul" >
     <img :src="원룸들[i].image" class="room-img"> <br>
     <h4>술 : {{원룸들[i].name}}</h4>
     <h4>회사명 :{{원룸들[i].company}}</h4>
     <h4>{{원룸들[i].type}}</h4>
     <h4>도수 : {{원룸들[i].density}}도</h4>
-
     <!-- <input type="checkbox" >{{원룸들[i].title}} <br> -->
     <!-- <h4 @click="모달창열림 = true" class="sul">{{원룸들[i].title}} </h4> -->
-    <p>{{원룸들[i].price}}원 </p>
+    <h4>{{원룸들[i].price}}원 </h4>
     <!-- <span>체크한 이름: {{ checkedNames }}</span> -->
     <button @click="구독수[i]='o' ">선택</button> <button @click="구독수[i]='x' ">취소</button> <span>구독 : {{구독수[i]}} </span>
     <hr>
@@ -91,7 +92,7 @@ div {
   width: 100%;
 }
 .room-img {
-  width: 30%;
+  width: 20%;
   margin-top: 10px;
 }
 #app {
@@ -104,12 +105,16 @@ div {
 }
 
 .menu {
-  background: darkslateblue;
+  background: skyblue;
   padding: 15px;
   border-radius: 5px;
 }
 .menu a {
   color: white;
   padding: 10px;
+}
+.jul h4 {
+  line-height: 10%;
+  /* padding: 0px; */
 }
 </style>
